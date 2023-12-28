0e87b9bc0640c35f4d1db31b00c9e9841dc44d4b
serhatprogramming <serhatprogramming@gmail.com>
2023-12-28 14:44:34 -0500

    query run for persons in App

--- src/App.js
+++ src/App.js
@@ -1,7 +1,22 @@
 import React from "react";
+import { gql, useQuery } from "@apollo/client";
+
+const ALL_PERSONS = gql`
+  query {
+    allPersons {
+      name
+      phone
+      id
+    }
+  }
+`;
 
 const App = () => {
-  return <div>Hello World!</div>;
+  const result = useQuery(ALL_PERSONS);
+  if (result.loading) {
+    return <div>loading...</div>;
+  }
+  return <div>{result.data.allPersons.map((p) => p.name).join(", ")}</div>;
 };
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,6 +1,39 @@
-import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 
+import {
+  ApolloClient,
+  ApolloProvider,
+  InMemoryCache,
+  gql,
+} from "@apollo/client";
+
+const client = new ApolloClient({
+  uri: "http://localhost:4000",
+  cache: new InMemoryCache(),
+});
+
+const query = gql`
+  query {
+    allPersons {
+      name
+      phone
+      address {
+        street
+        city
+      }
+      id
+    }
+  }
+`;
+
+client.query({ query }).then((response) => {
+  console.log(response.data);
+});
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<App />);
+root.render(
+  <ApolloProvider client={client}>
+    <App />
+  </ApolloProvider>
+);
