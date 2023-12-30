468a3e8cd19da4cb53fcccfda293e205654d40f7
serhatprogramming <serhatprogramming@gmail.com>
2023-12-30 16:12:13 -0500

    erro message added.

--- src/App.js
+++ src/App.js
@@ -1,18 +1,29 @@
-import React from "react";
+import { useState } from "react";
 import { useQuery } from "@apollo/client";
 import Persons from "./Persons";
 import PersonForm from "./PersonForm";
+import Notify from "./Notify";
 import { ALL_PERSONS } from "./queries";
 
 const App = () => {
+  const [errorMessage, setErrorMessage] = useState(null);
   const result = useQuery(ALL_PERSONS);
   if (result.loading) {
     return <div>loading...</div>;
   }
+
+  const notify = (message) => {
+    setErrorMessage(message);
+    setTimeout(() => {
+      setErrorMessage(null);
+    }, 5000);
+  };
+
   return (
     <div>
+      <Notify errorMessage={errorMessage} />
       <Persons persons={result.data.allPersons} />
-      <PersonForm />
+      <PersonForm setError={notify} />
     </div>
   );
 };
